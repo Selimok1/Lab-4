e90798c7824cc2a2d0ee6d833cf23e3f8c2bf8cd
Selim <ypselimok29@gmail.com>
2025-04-28 11:51:17 +0300

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,32 @@
+import random
+import time
+
+secret_number = random.randint(1, 100)
+
+start_time = time.time()
+
+attempts = 0
+
+print(" Я загадал число от 1 до 100. Попробуйте его угадать!")
+
+while True:
+    try:
+        user_guess = int(input(" Введите ваше предположение: "))
+        attempts += 1
+
+        if user_guess < secret_number:
+            print("🔼 Больше")
+        elif user_guess > secret_number:
+            print("🔽 Меньше")
+        else:
+            print(f" Поздравляю! Вы угадали число {secret_number} за {attempts} попыток.")
+            break
+    except ValueError:
+        print(" Пожалуйста, введите целое число!")
+end_time = time.time()
+elapsed_time = end_time - start_time
+
+print(f" Вы потратили {elapsed_time:.2f} секунд на игру.")
+
+with open("results.txt", "a", encoding="utf-8") as file:
+    file.write(f"Число: {secret_number}, Попытки: {attempts}, Время: {elapsed_time:.2f} секунд\n")
